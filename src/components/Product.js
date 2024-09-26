6eb17c3673a3c1a3451fcd0f92140ebac7d2dd01
Uneeba shaikh <148570196+UNEEBASHAIKH@users.noreply.github.com>
2024-09-26 21:20:42 +0500

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -29,7 +29,7 @@ const Product = () => {
     }, [productId]);
 
     return (
-        <div className="container" style={{ color: "rgb(102, 0, 51)" }}>
+        <div className="container" style={{ color: "rgb(166,204,190)" }}>
             {product ?
                 <div className="mt-5">
                     <div>
@@ -90,7 +90,7 @@ const Product = () => {
                 </div>
                 :
                 <div className="d-flex justify-content-center">
-                    <div className="spinner-border mt-5" role="status" style={{ color: "rgb(102, 0, 51)" }}>
+                    <div className="spinner-border mt-5" role="status" style={{ color: "rgb(166,204,190)" }}>
                         <span className="visually-hidden">Loading...</span>
                     </div>
                 </div>
